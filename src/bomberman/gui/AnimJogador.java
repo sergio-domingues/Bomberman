5b1e12b9dc43fcf745302e25a70f3ebd3f78ee89
Diogo Moura <diogomoura13@gmail.com>
2015-06-04 03:02:20 +0100

    Cada vez pior

---
+++ src/bomberman/gui/AnimJogador.java
@@ -0,0 +1,54 @@
+package bomberman.gui;
+
+import java.util.Observable;
+import java.util.Observer;
+
+import bomberman.connection.ConnectionId;
+import bomberman.logic.Jogador;
+import bomberman.logic.Jogador.Direcao;
+
+public class AnimJogador implements Observer {
+	private ConnectionId conn;
+
+	enum Instruction {
+		PLANTBOMB, MOVE, STOP
+	}
+
+	public Jogador.Direcao dir = null;
+	public Instruction nextInstruction = null;
+
+	public AnimJogador(ConnectionId conn) {
+		this.conn = conn;
+		conn.addObserver(this);
+	}
+
+	@Override
+	public void update(Observable arg0, Object arg1) {
+		String message = (String) arg1;
+
+		if (message.equals("moveLeftPressed")) {
+			nextInstruction = Instruction.MOVE;
+			dir = Direcao.ESQUERDA;
+		} else if (message.equals("moveLeftReleased")) {
+			nextInstruction = Instruction.STOP;
+		} else if (message.equals("moveRightPressed")) {
+			nextInstruction = Instruction.MOVE;
+			dir = Direcao.DIREITA;
+		} else if (message.equals("moveRightReleased")) {
+			nextInstruction = Instruction.STOP;
+		} else if (message.equals("moveUpPressed")) {
+			nextInstruction = Instruction.MOVE;
+			dir = Direcao.CIMA;
+		} else if (message.equals("moveUpReleased")) {
+			nextInstruction = Instruction.STOP;
+		} else if (message.equals("moveDownPressed")) {
+			nextInstruction = Instruction.MOVE;
+			dir = Direcao.BAIXO;
+		} else if (message.equals("moveDownReleased")) {
+			nextInstruction = Instruction.STOP;
+		} else if (message.equals("plantBomb")) {
+			nextInstruction = Instruction.PLANTBOMB;
+		}
+		
+	}
+}
